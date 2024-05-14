6b52d914b8c47b6b9b1c1a56f98ebb0cf4419ee5
Federico Williamson <federicowilliamson@hotmail.co.uk>
2024-05-14 14:46:32 -0300

    Re-format SRC

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,17 @@
 using namespace std;
 
 int main() {
-    // load_file("c2-orthogonal.rle");
-    Canvas<9, 9> canvas{};
-    canvas.init();
-    canvas.spawn(
-            ".X..X\n"
-            "X....\n"
-            "X...X\n"
-            "XXXX.\n",
-            0, 0
-    );
-    canvas.print();
+	// load_file("c2-orthogonal.rle");
+	Canvas<9, 9> canvas{};
+	canvas.init();
+	canvas.spawn(
+		".X..X\n"
+		"X....\n"
+		"X...X\n"
+		"XXXX.\n",
+		0, 0
+	);
+	canvas.print();
 //    canvas.load_file("../data/mini.rle");
 //    cout << CLEAR_SCREEN << flush;
 //    for (int i=0; i<100; i++){
@@ -27,5 +27,5 @@ int main() {
 //    canvas.generate(lb, 0, true, 4);
 //    canvas.generate(lb, 0, false, 1);
 //    canvas.generate(lb, 0, true, 1);
-    canvas.print();
+	canvas.print();
 }
--- src/utils.hxx
+++ src/utils.hxx
@@ -2,16 +2,17 @@
 #define GOL_UTILS_HXX
 
 constexpr int clamp(int v, int m, int M) {
-    return v < m ? m : (v > M ? M : v);
+	return v < m ? m : (v > M ? M : v);
 }
 
 template<int width>
 constexpr int coords(int x, int y) {
-    return y * width + x;
+	return y * width + x;
 }
+
 template<int width, int height>
 constexpr int clamped_coords(int x, int y) {
-    return coords<width>(clamp(x, 0, width - 1), clamp(y, 0, height - 1));
+	return coords<width>(clamp(x, 0, width - 1), clamp(y, 0, height - 1));
 }
 
 #endif //GOL_UTILS_HXX
